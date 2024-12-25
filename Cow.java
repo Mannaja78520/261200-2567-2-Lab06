d239792ab0676c795e2b5bd9adee70945b7ebe9a
Mannaja <manman7852078520@gmail.com>
2024-12-25 17:36:00 +0700

    finished

---
+++ Cow.java
@@ -0,0 +1,9 @@
+public class Cow extends Animal{
+    @Override
+    void sound() {
+        System.out.println("moo moo (มูมู)");
+    }
+    void milk() {
+        System.out.println("... now loading ...");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args){
+        Cow wagyu = new Cow();
+        Duck donald = new Duck("โดนัลด์");
+        Duck daisy = new Duck("เดซี่");
+        Pig burin = new Pig();
+        Owl hedwig = new Owl();
+        // wagyu.sound();
+        // donald.sound();
+        // burin.sound();
+        // hedwig.sound();
+        donald.clean(wagyu);
+        daisy.clean(donald);
+        PekingDuck jiaTongHeng = new PekingDuck("JTH");
+        jiaTongHeng.clean(daisy);
+        jiaTongHeng.fly();
+
+        donald.clean(new Cow());
+        donald.clean(new Cow());
+        donald.clean(new Cow());
+        donald.clean(new Cow());
+    }
+}
